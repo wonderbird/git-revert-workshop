a9d216e3df05659b58ab190328f72d4647c5e1c9
Stefan Boos <kontakt@boos.systems>
2025-06-13 19:39:25 +0200

    feat: show last 3 deployed workflows

--- app/main.js
+++ app/main.js
@@ -1,7 +1,31 @@
-// TODO: Insert your application logic here
 (async () => {
-  let title = document.querySelector('h1');
-  if (title) {
-    title.textContent = 'Hello, World!';
-  }
+  listRecentlyDeployedWorkflows();
 })();
+
+function listRecentlyDeployedWorkflows() {
+  const numberOfWorkflows = 3;
+
+  fetch('/workflows.txt')
+    .then(response => response.text())
+    .then(data => {
+      const workflows = data.split('\n')
+                            .filter(line => line.trim() !== '')
+                            .reverse()
+                            .slice(0, numberOfWorkflows);
+
+      workflows.forEach(workflow => {
+        displayWorkflow(workflow);
+      });
+    });
+}
+
+function displayWorkflow(workflow) {
+  const workflowList = document.getElementById('workflows');
+
+  if (workflowList) {
+    const listItem = document.createElement('li');
+    listItem.className = 'list-group-item';
+    listItem.textContent = workflow;
+    workflowList.appendChild(listItem);
+  }
+}
